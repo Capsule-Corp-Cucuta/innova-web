39f808a95deabbdde521c9baf12d5bcee0eb5736
Guillermo Parada <jguillermoparada@gmail.com>
2021-05-15 23:36:17 -0500

    feat[event-module] se añaden vistas para registrar,consultar,actualizar y ver eventos

--- src/app/features/event/table/table.component.ts
+++ src/app/features/event/table/table.component.ts
@@ -1,8 +1,57 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { LabelConstants } from 'src/app/shared/constants/label-constants';
+import { UrlConstants } from 'src/app/shared/constants/url-constants';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['../../../shared/styles/_table.component.scss'],
 })
-export class TableComponent {}
+export class TableComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  public readonly ICONS = LabelConstants.ICONS;
+  public readonly ROUTES = UrlConstants.ROUTES;
+  public readonly LABELS = LabelConstants.LABELS.EVENT.LIST;
+
+  public events: [] = [];
+  public event: MatTableDataSource<[]>;
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  ngAfterViewInit(): void {
+    //this.event.sort = this.sort;
+    //this.event.paginator = this.paginator;
+  }
+
+  public applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.event.filter = filterValue.trim().toLowerCase();
+
+    if (this.event.paginator) {
+      this.event.paginator.firstPage();
+    }
+  }
+
+  public openDialog(event: string): void {
+    if (event) {
+      this.dialog.open(ModalComponent, {
+        data: event,
+      });
+    }
+  }
+
+  private loadData(): void {
+    //TODO
+  }
+}
