2ba95409d46cc6efca6e0842e00ba34fb55e382d
Sergio Rodriguez Ramirez <sergioarodram@gmail.com>
2021-06-05 22:14:38 -0500

    feat[firebase-hosting] solve unauthorized endpoints

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,5 @@
 export const environment = {
   production: true,
-  APIUrl: 'https://innova-ufps.herokuapp.com/innova/api/',
-  firebase: {
-    apiKey: 'AIzaSyDWU0EcvZVxbazim6OeYDU7Dryk1MRPvd4',
-    authDomain: 'innova-ufps.firebaseapp.com',
-    projectId: 'innova-ufps',
-    storageBucket: 'innova-ufps.appspot.com',
-    messagingSenderId: '650800187777',
-    appId: '1:650800187777:web:232d70e4785ec52139c080',
-  },
+  APIUrl: 'http://localhost:8080/innova/api/',
+  firebase: {},
 };
